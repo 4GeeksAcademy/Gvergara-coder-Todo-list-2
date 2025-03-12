39e4ae2de24991c386651d458e60c3ce2f0d04d6
Gabriel Vergara <69174732+Gvergara-coder@users.noreply.github.com>
2025-03-12 01:16:36 +0000

    [Se creo el Todolist] se corrigio el ccs del Li se agrego un nuevo useState

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,12 +3,12 @@ import React, {useState} from "react";
 //create your first component
 const Home = () => {
 	const [tasks, setTasks] = useState(["Make the bed", "Whas my hands", "Eat", "Walk the dog"]);
-	/*const [newTask, setNewTask] = useState ("");*/
+	const [newTask, setNewTask] = useState ("");
 
 	function handleKeyPress(event){
 		
-		if (event.key == "Enter" && event.target.value != ""){
-			setTasks([...tasks, event.target.value])
+		if (event.key == "Enter" && newTask != ""){
+			setTasks([...tasks, newTask])
 		} 
 	}
 	function deleteTask(index){
@@ -21,15 +21,18 @@ const Home = () => {
 			<h1>To-Do-List</h1>
 
 			<div>
-				<input type="text" 
+				<input
+				value={newTask}
+				type="text" 
 				placeholder="Enter a task..."
 				onKeyDown={handleKeyPress}
+				onChange={(e)=> setNewTask(e.target.value)}
 				/>
 			</div>
 
 			<ol>
 				{tasks.map((task, index) =>
-					<li key={index}>
+					<li key={index} className="task-hidden">
 						<span className="text">{task}</span>
 						<button
 							className="delete-button"
